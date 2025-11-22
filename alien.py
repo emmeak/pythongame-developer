a85000c8cdab787c5b1fce0fb4abd04026b04c42
emmeak <emme.ak09@gmail.com>
2025-11-22 08:53:46 +0000

    Add files via upload

--- alien.py
+++ alien.py
@@ -2,13 +2,24 @@ import random
 import pgzrun
 TITLE = "ATTACK the little alien!"
 WIDTH = 605
-HIGHT = 506
+HEIGHT = 506
 littlealien = Actor("littlealien")
 littlealien.pos = 390,231
+
+message = ""
 
 def draw():
     screen.clear()
     screen.fill("dark blue")
     littlealien.draw()
+    screen.draw.text(message, center = (477,12), fontname = "gabriola.ttf", fontsize = 29)
 
+def on_mouse_down(pos):
+    global message
+    if littlealien.collidepoint(pos):
+        littlealien.x = random.randint(50,550)
+        littlealien.y = random.randint(40,440)
+        message = "Winners get 2125!"
+    else:
+        message = "Losers get 4963..."
 pgzrun.go()

d4699de7125e0c5be9a2f0ca061d1a69ee1eb2c6
emmeak <emme.ak09@gmail.com>
2026-03-14 08:58:10 +0000

    Add files via upload

--- babys.py
+++ babys.py
@@ -13,6 +13,8 @@ for i in range(12):
 
 starttime = 0
 totaltime = 0
+next_sat = 0
+total_sat = 12
 
 def draw():
     screen.blit("thebigspace", (0,0))
@@ -21,8 +23,22 @@ def draw():
         i.draw()
         screen.draw.text(str(bbsn), (i.pos[0], i.pos[1] + 20))
         bbsn +=1
+    for i in satlines:
+        screen.draw.line(i[0], i[1], "white")
 
 def update():
     pass
+
+satlines = []
+def on_mouse_down(pos):
+    global next_sat, satlines
+    if next_sat < total_sat:
+        if satellite[next_sat].collidepoint(pos):
+            if next_sat:
+                satlines.append((satellite[next_sat-1].pos, satellite[next_sat].pos))
+            next_sat += 1
+        else:
+            satlines = []
+            next_sat = 0
 
 pgzrun.go()
